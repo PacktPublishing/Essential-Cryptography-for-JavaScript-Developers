b22188f2c90921bd52f90e79503b3ebfc1b4ddeb
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2021-06-02 22:30:42 -0700

    More code samples

---
+++ ch4-symmetric-encryption/argon2-kdf.js
@@ -0,0 +1,64 @@
+// Import the argon2 module
+// https://www.npmjs.com/package/argon2
+const argon2 = require('argon2')
+
+/**
+ * Derive a key from a given passphrase and salt, using Argon2 with explicit parameters.
+ * @param {string} passphrase The passphrase to derive the key from
+ * @param {Buffer} salt The salt used for this key; should be 16-byte long
+ * @param {number} length Number of bytes to return from the hash (should be 16 for a 128-bit key or 32 for a 256-bit key)
+ * @returns {Promise<Buffer>} The symmetric key derived with Argon2
+ */
+function deriveKey(passphrase, salt, length) {
+    try {
+        // Parameters for argon2
+        const params = {
+            // Set to return the raw bytes and not a base64-encoded hash (as we'd use for passwords instead)
+            raw: true,
+            // Key length
+            hashLength: length,
+            // Pass the salt
+            salt: salt,
+            // We need to make them all parameters explicit just in case the default ones in the library changed.
+            // In fact, if that happened, the same passphrase and salt would return a different key and we wouldn't be able to decrypt our data encrypted with that key and salt combination.
+            // You can tweak these as needed.
+            type: argon2.argon2id,
+            timeCost: 3,
+            memoryCost: 4096,
+            parallelism: 1,
+            version: 0x13,
+        }
+        // Derive and return the key
+        return argon2.hash(passphrase, params)
+    }
+    catch (err) {
+        console.error('An internal error occurred: ', err)
+    }
+}
+
+/* Example usage */
+
+// Wrap in an asynchronous IIFE (Immediately-Invoked Function Expression) because we need to use the await keyword
+;(async function() {
+    // Passphrase to use
+    const passphrase = 'correct horse battery staple'
+
+    // Salt: this should ideally be different for every file and stored together with the encrypted file
+    // Recommended length is 16 bytes
+    // Here it's hardcoded for our example, and it's base64-encoded so we need to decode it
+    const salt = Buffer.from('WiHmGLjgzYESy3eAW45W0Q==', 'base64')
+
+    // Derive a 128-bit key (16 bytes in length)
+    const key128 = await deriveKey(passphrase, salt, 16)
+    console.log('128-bit key:', key128.toString('base64'))
+
+    // Derive a 256-bit key (32 bytes in length)
+    const key256 = await deriveKey(passphrase, salt, 32)
+    console.log('256-bit key:', key256.toString('base64'))
+})()
+
+/*
+Result:
+  128-bit key: McvSLprU4zfh1kcVOeR40g==
+  256-bit key: oQumof86t+UlE6yBPCbblO6IcPmrL8qHj/jucYIxJFw=
+*/
---
+++ ch4-symmetric-encryption/chacha20-poly1305.js
@@ -0,0 +1,86 @@
+// Import the required modules
+const crypto = require('crypto')
+
+// Promisify the randomBytes method
+const randomBytes = require('util').promisify(crypto.randomBytes)
+
+/**
+ * Encrypts the plaintext message using the given key with ChaCha20-Poly1305
+ * @param {Buffer} key A 256-bit key
+ * @param {string} plaintext The message to encrypt, as a string
+ * @returns {Buffer} The ciphertext (with the nonce and authentication tag prepended)
+ */
+async function encrypt(key, plaintext) {
+    // Generate a random nonce of 12 bytes
+    const nonce = await randomBytes(12)
+
+    // Encrypt the message
+    const cipher = crypto.createCipheriv('chacha20-poly1305', key, nonce, {
+        authTagLength: 16
+    })
+    const encrypted = Buffer.concat([
+        cipher.update(plaintext, 'utf8'),
+        cipher.final()
+    ])
+
+    // Prepend the nonce and the authentication tag to the ciphertext
+    // This is the data that should be stored or transmitted
+    const tag = cipher.getAuthTag()
+    return Buffer.concat([nonce, tag, encrypted])
+}
+
+/**
+ * Decrypts the encrypted message using the given key with ChaCha20-Poly1305
+ * @param {Buffer} key The 256-bit key used to encrypt the message
+ * @param {Buffer} message The ciphertext (with the nonce and authentication tag prepended)
+ * @returns {string} The decrypted message
+ */
+function decrypt(key, message) {
+    // The first 12 bytes in the encrypted message are the nonce
+    // Next 16 bytes are the authentication tag
+    // The rest is the ciphertext
+    const nonce = message.slice(0, 12)
+    const tag = message.slice(12, 28)
+    const ciphertext = message.slice(28)
+
+    // Decrypt the ciphertext
+    const decipher = crypto.createDecipheriv('chacha20-poly1305', key, nonce, {
+        authTagLength: 16
+    })
+    decipher.setAuthTag(tag)
+    const decrypted = Buffer.concat([
+        decipher.update(ciphertext, 'utf8'),
+        decipher.final()
+    ])
+
+    return decrypted.toString('utf8')
+}
+
+/* Example usage */
+
+// Wrap in an asynchronous IIFE (Immediately-Invoked Function Expression) because we need to use the await keyword
+;(async () => {
+    // Message to encrypt
+    const plaintext = 'Hello world!'
+
+    // Key to use
+    // In this case, we're generating a random 256-bit (32 byte) key
+    // We're printing the key (base64-encoded) as example
+    const key = await randomBytes(32)
+    console.log('Key:', key.toString('base64'))
+
+    // Encrypt the message and show the result (base64-encoded)
+    const encrypted = await encrypt(key, plaintext)
+    console.log('Encrypted message:', encrypted.toString('base64'))
+
+    // Decrypt the encrypted message and show the result
+    const decrypted = decrypt(key, encrypted)
+    console.log('Decrypted message:', decrypted)
+})()
+
+/*
+Example result (will be different every time):
+  Key: pG0P+/xXa6nFWG+X9WcAO4uIBWCTViSEIyiZBc9JyJA=
+  Encrypted message: Ab/Rr9FSkn69HxOU96we7DNNM7ntSChfVWyRXjvburtXVRa+Pvienw==
+  Decrypted message: Hello world!
+*/
