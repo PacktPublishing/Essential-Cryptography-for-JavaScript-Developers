158bb6aefc062ae309db3a0f1dd0c4b19593664c
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2021-08-30 20:16:03 -0700

    Added code samples for ECIES

---
+++ ch5-asymmetric-and-hybrid-encryption/ecies-server.js
@@ -0,0 +1,111 @@
+// Import the express module to create a web server
+// https://www.npmjs.com/package/express
+// Note: requires express 4.16+
+const express = require('express')
+
+// Import the required modules
+const crypto = require('crypto')
+const {promisify} = require('util')
+
+// Promisify the crypto.generateKeyPair method
+const generateKeyPair = promisify(crypto.generateKeyPair)
+
+// Dictionary that stores all ephemeral private keys used for the ECDH key agreement
+// We're keeping the keys in memory just for this example in production, you will probably want to store the keys in a database and purge private keys after a certain amount of time, whether the key agreement was completed or not
+const keyPairs = {}
+
+// Create a web server using Express
+const app = express()
+
+// Handler for GET /public-key
+// This generates a new key pair, saves the private key in memory and returns the public part to the client
+app.get('/public-key', async (req, res) => {
+    // Generate a new x25519 key pair
+    const newKeyPair = await generateKeyPair('x25519')
+    // Export the public key encoded as PEM
+    const publicKey = newKeyPair.publicKey.export({
+        type: 'spki',
+        format: 'pem'
+    })
+    // Calculate a "key ID", which in this simplified scenario is just the SHA-256 hash of the PEM-encoded public key
+    const keyId = crypto.createHash('sha256')
+        .update(publicKey)
+        .digest('base64url')
+    // Store the key pair in the dictionary, as we'll need the private key to complete the key agreement
+    keyPairs[keyId] = newKeyPair
+    // Return the (PEM-encoded) public key to the client and the key ID
+    res.send({
+        keyId,
+        publicKey
+    })
+})
+
+// Handler for POST /message
+// This receives an encrypted message (message) using ECIES (using the format defined in ecies-client.js) as well as the client's public key (publicKey) and the ID of the private key the server (serverKeyId) generated in the /public-key step
+// It decrypts the message received and shows it in the console
+// This request expects input in JSON format in the POST body
+app.post('/message', express.json(), (req, res) => {
+    // Get the message from the input; this is encoded as base64, so it has to be decoded
+    if (!req.body.message) {
+        throw Error(`Missing parameter 'message'`)
+    }
+    const message = Buffer.from(req.body.message, 'base64')
+
+    // Get the client's public key from the input
+    const clientPublicKey = req.body.publicKey
+    if (!clientPublicKey) {
+        throw Error(`Missing parameter 'publicKey'`)
+    }
+    // Get the server's key ID from the input
+    const keyId = req.body.serverKeyId
+    if (!keyId) {
+        throw Error(`Missing parameter 'serverKeyId'`)
+    }
+    const serverKeyPair = keyPairs[keyId]
+    if (!serverKeyPair) {
+        throw Error(`Key pair with ID ${keyId} not found`)
+    }
+
+    // Calculate the shared secret
+    const sharedSecret = crypto.diffieHellman({
+        publicKey: crypto.createPublicKey(clientPublicKey),
+        privateKey: serverKeyPair.privateKey
+    })
+    // Remove the key from memory
+    delete keyPairs[keyId]
+
+    // The first 16 bytes of the encryptedMessage contain the salt for the KDF, which was used to stretch the sharedSecret into a symmetricKey
+    const salt = message.slice(0, 16)
+    const symmetricKey = crypto.createHash('sha256')
+        .update(Buffer.concat([sharedSecret, salt]))
+        .digest()
+
+    // The remaining bytes are the message encrypted with AES-256-GCM (including the IV and authentication tag)
+    const decrypted = aesDecrypt(symmetricKey, message.slice(16))
+
+    // Log the decrypted message
+    console.log('The decrypted message is', decrypted)
+
+    // Send an OK to the client
+    res.send('OK')
+})
+
+// Start the web server, listening on port 3000
+app.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000')
+})
+
+/** Example symmetric decryption function from aes-256-gcm */
+
+function aesDecrypt(key, message) {
+    const iv = message.slice(0, 12)
+    const tag = message.slice(12, 28)
+    const ciphertext = message.slice(28)
+    const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv)
+    decipher.setAuthTag(tag)
+    const decrypted = Buffer.concat([
+        decipher.update(ciphertext, 'utf8'),
+        decipher.final()
+    ])
+    return decrypted.toString('utf8')
+}
